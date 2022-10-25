b51aecec2fefd1d5e7a34cfd5db3799a69fcf361
JeyDev310 <mtroz310@gmail.com>
2022-10-26 04:29:57 +0800

    fix: update root api

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Athlete profile APIs" });
 });
 
 require("./app/routes/index.routes")(app);
